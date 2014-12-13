a6d8a57fb21ccd5c1ee4d2094ffb48435e7845cf
Adil <bonoadil@gmail.com>
2014-12-13 14:39:03 +0100

    init commit

--- src/com/mobirebel/scalandroid/Main.java
+++ src/com/mobirebel/scalandroid/Main.java
@@ -12,5 +12,6 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
     }
 }
